8fc23bda87813727a8533554bc820fe343c6ae7e
Cornelius <bcornelius@iseatz.com>
2020-08-12 22:39:56 -0500

    styles the clients button

--- src/app/admin/components/topic-card/topic-card.component.ts
+++ src/app/admin/components/topic-card/topic-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { IGradedItem } from 'src/app/shared/models/i-graded-item';
 
 @Component({
   selector: 'admin-topic-card',
@@ -10,8 +11,13 @@ export class TopicCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.itemKey = Object.keys(this.item)[0];
+    this.itemValue = Object.values(this.item)[0].concat("%");
   }
 
-  @Input() item: any = ["mmm", "54"];
+  @Input() item: IGradedItem = {mmm: "54"};
+  @Input() active: boolean;
 
+  itemKey: string;
+  itemValue: string;
 }
