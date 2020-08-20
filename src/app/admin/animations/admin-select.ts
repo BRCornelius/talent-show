b8f429a894381be5e609275b8c8c82dc47332a91
Cornelius <bcornelius@iseatz.com>
2020-08-19 21:41:44 -0500

    closes select on choice

--- src/app/admin/animations/admin-select.ts
+++ src/app/admin/animations/admin-select.ts
@@ -2,7 +2,7 @@ import { trigger, transition, style, animate, state } from '@angular/animations'
 
 export const adminSelect = trigger('toggleAdmin', [
     state('open', style({
-      backgroundColor: 'rgb(219, 23, 23)',
+      backgroundColor: '#c50c0d',
       'border-top-left-radius': '0.75rem',
       'border-top-right-radius': '0.75rem',
       color: 'white'
